e9ef2dd3fd9d51e133f87d81067e7cbacca69789
Nick Owens <owensnick@gmail.com>
2023-04-19 18:48:14 +0100

    added deps

--- src/SMFTools.jl
+++ src/SMFTools.jl
@@ -1,10 +1,12 @@
 module SMFTools
 
-using BioAlignments, BioSequences, XAM, GenomicFeatures, GenomeFragments
+using DataFrames, CSV
+using BioAlignments, BioSequences, XAM, GenomicFeatures, GenomeFragments, FASTX
 using ProgressMeter
+using Statistics, StatsBase
 
-export streambam_smf, validfrag, streamfrags, posmlp
+export streambam_smf, validfrag, streamfrags, posmlp, bam_meth_stats
 
 include("stream.jl")
-
+include("readstats.jl")
 end
