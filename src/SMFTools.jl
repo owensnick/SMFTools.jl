396fa4886de285ddf2cadae72627e246527c03c7
Nick Owens <owensnick@gmail.com>
2024-05-23 18:00:08 +0100

    updated deps and exports

--- src/SMFTools.jl
+++ src/SMFTools.jl
@@ -3,10 +3,16 @@ module SMFTools
 using DataFrames, CSV
 using BioAlignments, BioSequences, XAM, GenomicFeatures, GenomeFragments, FASTX
 using ProgressMeter
-using Statistics, StatsBase
+using Statistics, StatsBase, OnlineStats
+using Mmap, UnalignedVectors
+using Parsers
 
-export streambam_smf, validfrag, streamfrags, posmlp, bam_meth_stats
+using Blosc
+
+export streambam_smf, validfrag, streamfrags, posmlp, bam_meth_stats, load_pos_matrix, getreadindex, getposview, modmeta, modheat
 
 include("stream.jl")
 include("readstats.jl")
+include("posmatrix.jl")
+include("processing.jl")
 end
